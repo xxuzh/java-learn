244e228934c00eb6a30fc30f4a15733d56cde204
ZhouXUU <zhou.xuu@outlook.com>
2018-09-14 21:07:16 +0200

    gut test

---
+++ test1/src/helloworld.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2018-2019, XZ
+ * FileName: helloworld
+ * Author:   kaine
+ * Date:     2018/9/14 20:50
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author kaine
+ * @create 2018/9/14
+ * @since 1.0.0
+ */
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("aasdfsfa");
+    }
+
+}
---
+++ test1/src/myfirstapp.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2018-2019, XZ
+ * FileName: myfirstapp
+ * Author:   kaine
+ * Date:     2018/9/14 21:01
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author kaine
+ * @create 2018/9/14
+ * @since 1.0.0
+ */
+public class myfirstapp {
+    public static void main(String[] args){
+        System.out.println("aasdfsfa");
+        System.out.println("1");
+    }
+}
